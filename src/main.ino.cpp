01a55c3a608dc6bb35936d074c2bda6a747a9dbe
escri11 <escri11@yahoo.es>
2017-03-22 16:40:49 +0100

    Llum blanca

--- src/main.ino.cpp
+++ src/main.ino.cpp
@@ -1,6 +1,12 @@
+# 1 "/var/folders/z0/zn0w3_z16k748s0bjj55w3fr0000gn/T/tmp8YyQL3"
+#include <Arduino.h>
+# 1 "/Users/Escriba/Documents/ETSEIAT/Curs Arduino/P1/src/main.ino"
 int led = 2;
 int temps = 10;
-
+void setup();
+void loop ();
+int waitforit (int espera);
+#line 4 "/Users/Escriba/Documents/ETSEIAT/Curs Arduino/P1/src/main.ino"
 void setup() {
   pinMode(led, OUTPUT);
 }
