7e46239d782ee22f43a3471fa66ac04eaadb4d2e
escri11 <escri11@yahoo.es>
2017-03-22 16:31:34 +0100

    RGB led aplication

--- src/main.ino
+++ src/main.ino
@@ -1,4 +1,4 @@
-int led = 13;
+int led = 2;
 int temps = 100;
 
 void setup() {
@@ -9,7 +9,12 @@ void loop () {
   digitalWrite(led, HIGH);
   waitforit(temps);
   digitalWrite(led, LOW);
+  digitalWrite(led+1, HIGH);
   waitforit(temps);
+  digitalWrite(led+1, LOW);
+  digitalWrite(led+2, HIGH);
+  waitforit(temps);
+  digitalWrite(led+2, LOW);
 }
 
 int waitforit (int espera) {
