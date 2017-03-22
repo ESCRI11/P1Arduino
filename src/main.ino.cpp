8f7ada9bcb720479c399f8a289b2cc8517a72dea
escri11 <escri11@yahoo.es>
2017-03-22 17:28:36 +0100

    Variacio de colorillos

--- src/main.ino.cpp
+++ src/main.ino.cpp
@@ -1,9 +1,14 @@
+# 1 "/var/folders/z0/zn0w3_z16k748s0bjj55w3fr0000gn/T/tmpZToN3R"
+#include <Arduino.h>
+# 1 "/Users/Escriba/Documents/ETSEIAT/Curs Arduino/P1/src/main.ino"
 int led = 4;
 int lednocurrent = 5;
 int inputpin = 6;
 int val = 0;
 int valorreal = 0;
-
+void setup();
+void loop ();
+#line 7 "/Users/Escriba/Documents/ETSEIAT/Curs Arduino/P1/src/main.ino"
 void setup() {
   pinMode(led, OUTPUT);
   pinMode(inputpin, INPUT);
