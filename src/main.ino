4d8e19d6c8e5e62ab0d72301177ae925a1aeb0fe
escri11 <escri11@yahoo.es>
2017-04-09 22:54:50 +0200

    Versio treball final

--- src/main.ino
+++ src/main.ino
@@ -4,6 +4,8 @@ int led3 = 9;
 int i = 0;
 int j = 0;
 int k = 0;
+float alfa = 0;
+float pi = 3.14;
 
 void setup() {
   pinMode(led1, OUTPUT);
@@ -12,28 +14,12 @@ void setup() {
 }
 
 void loop () {
-      for (i = 0; i < 255; i++) {
-        analogWrite(led1, i);
-        delay(5);
-      }
-      for (i = 255; i > 0; i--) {
-        analogWrite(led1, i);
-        delay(5);
-      }
-      for (j = 85; j < 255; j++) {
-        analogWrite(led2, j);
-        delay(5);
-      }
-      for (j = 255; j > 0; j--) {
-        analogWrite(led2, j);
-        delay(5);
-      }
-      for (k = 170; k < 255; k++) {
-        analogWrite(led3, i);
-        delay(5);
-      }
-      for (k = 255; k > 0; k--) {
-        analogWrite(led3, i);
-        delay(5);
-      }
+  i = (128 * sin (alfa/100)) + 128;
+  j = (128 * sin ((alfa/100) + (2 * pi / 3))) + 128;
+  k = (128 * sin ((alfa/100) + (4 * pi / 3))) + 128;
+  delay (5);
+  analogWrite(led1, i);
+  analogWrite(led2, j);
+  analogWrite(led3, k);
+  alfa ++;
 }
