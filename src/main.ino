b6dedb4da35f0619c851ab471ca64ac8e0dba5e2
escri11 <escri11@yahoo.es>
2017-04-13 15:17:02 +0200

    Versió entregada, amb serial input

--- src/main.ino
+++ src/main.ino
@@ -4,6 +4,7 @@ int led3 = 9;
 int i = 0;
 int j = 0;
 int k = 0;
+int retard = 0;
 float alfa = 0;
 float pi = 3.14;
 
@@ -11,15 +12,20 @@ void setup() {
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
   pinMode(led3, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop () {
+  if (Serial.available() > 0) {
+    retard = Serial.parseInt();
+  }
   i = (128 * sin (alfa/100)) + 128;
   j = (128 * sin ((alfa/100) + (2 * pi / 3))) + 128;
   k = (128 * sin ((alfa/100) + (4 * pi / 3))) + 128;
-  delay (5);
+  delay (retard);
   analogWrite(led1, i);
   analogWrite(led2, j);
   analogWrite(led3, k);
   alfa ++;
+  if (alfa > 1280) {alfa = 0;}
 }
